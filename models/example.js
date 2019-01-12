7480a15900bdaa1bb8a9a527fa118a95bc4b7995
Ordolph <austin.t.randolph@gmail.com>
2019-01-12 12:27:34 -0500

    Built model for player table

--- models/example.js
+++ models/example.js
@@ -1,7 +1,32 @@
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
+module.exports = function (sequelize, DataTypes) {
+  var Player = sequelize.define("Player", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+      validate: {
+        len: [1, 50]
+      },
+
+      icon: {
+        type: DataTypes.TEXT,
+      },
+
+      plays: {
+        type: DataTypes.MEDIUMINT,
+        allowNull: false,
+        defaultVaulue: 0
+      },
+      wins: {
+        type: DataTypes.MEDIUMINT,
+        allowNull: false,
+        defaultValue: 0
+      },
+      losses: {
+        type: DataTypes.MEDIUMINT,
+        allowNull: false,
+        defaultValue: 0
+      }
+    });
+  return Player;
 };
