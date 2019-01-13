19b4defd35390b5bcd2d684e5594725fd3a6c8d9
Ordolph <austin.t.randolph@gmail.com>
2019-01-13 12:11:36 -0500

    Fixed indenting issue

--- models/player.js
+++ models/player.js
@@ -1,31 +1,31 @@
-module.exports = function(sequelize, DataTypes) {
-  var Player = sequelize.define("Player", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+module.exports = function (sequelize, DataTypes) {
+    var Player = sequelize.define("Player", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
 
-      validate: {
-        len: [1, 50]
-      }
-    },
-    icon: {
-      type: DataTypes.TEXT
-    },
-    plays: {
-      type: DataTypes.MEDIUMINT,
-      allowNull: false,
-      defaultVaulue: 0
-    },
-    wins: {
-      type: DataTypes.MEDIUMINT,
-      allowNull: false,
-      defaultValue: 0
-    },
-    losses: {
-      type: DataTypes.MEDIUMINT,
-      allowNull: false,
-      defaultValue: 0
-    }
-  });
-  return Player;
+            validate: {
+                len: [1, 50]
+            }
+        },
+        icon: {
+            type: DataTypes.TEXT
+        },
+        plays: {
+            type: DataTypes.MEDIUMINT,
+            allowNull: false,
+            defaultVaulue: 0
+        },
+        wins: {
+            type: DataTypes.MEDIUMINT,
+            allowNull: false,
+            defaultValue: 0
+        },
+        losses: {
+            type: DataTypes.MEDIUMINT,
+            allowNull: false,
+            defaultValue: 0
+        }
+    });
+    return Player;
 };
